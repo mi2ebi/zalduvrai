b246b77d200150f8f2e16bd6cbebc6a494ee9fff
mi2ebi <merrybot47@gmail.com>
2025-07-25 14:30:47 -0700

    fmt

--- helper.js
+++ helper.js
@@ -1,68 +1,69 @@
 const $ = x => document.getElementById(x);
 function mkel(tag, props, children) {
-    const element = document.createElement(tag);
-    Object.assign(element, props);
-    for (const child of children) {
-        if (child) {
-            element.append(child);
-        }
+  const element = document.createElement(tag);
+  Object.assign(element, props);
+  for (const child of children) {
+    if (child) {
+      element.append(child);
     }
-    return element;
+  }
+  return element;
 }
 function htmlify(json) {
-    const entry = mkel("div", {"className": "entry"}, [
-        mkel("dt", null, [
-            mkel("a", {"href": "?q=" + json.head, "className": "head"
-            }, [
-                mkel("b", null, [json.head])
-            ]),
-            " ",
-            json.djifoa ? mkel("i", {"className": "djifoa"}, [
-                json.djifoa.join(" ")
-            ]) : null,
-            " ",
-            json.etym ? mkel("span", {"className": "etym"}, [
-                "← ", json.etym
-            ]) : null
-        ]),
-        json.defs ? json.defs.map(def => {
-            return [
-                mkel("i", {}, [def.pos + ". "]),
-                def.frame ? mkel("code", {}, ["[" + def.frame + "]"]) : null,
-                " ",
-                def.body,
-                mkel("br", {}, [])
-            ];
-        }) : null,
-        json.derivs ? json.derivs.map(deriv => {
-            return [
-                mkel("b", {}, [deriv.head]),
-                " ",
-                mkel("i", {}, [deriv.pos + ". "]),
-                deriv.body,
-                mkel("br", {}, [])
-            ];
-        }) : null,
-        json.used_in ? mkel("details", {}, [
-            mkel("summary", {}, ["used in (", json.used_in.length, ")"]),
-            json.used_in.map(u => {
-                return [
-                    mkel("a", {"href": "?q=" + u}, [u]),
-                    ", "
-                ];
-            })
-        ].flat(Infinity).slice(0, -1)) : null
-    ].flat(Infinity));
-    return entry;
+  const entry = mkel("div", { "className": "entry" }, [
+    mkel("dt", null, [
+      mkel("a", {
+        "href": "?q=" + json.head, "className": "head"
+      }, [
+        mkel("b", null, [json.head])
+      ]),
+      " ",
+      json.djifoa ? mkel("i", { "className": "djifoa" }, [
+        json.djifoa.join(" ")
+      ]) : null,
+      " ",
+      json.etym ? mkel("span", { "className": "etym" }, [
+        "← ", json.etym
+      ]) : null
+    ]),
+    json.defs ? json.defs.map(def => {
+      return [
+        mkel("i", {}, [def.pos + ". "]),
+        def.frame ? mkel("code", {}, ["[" + def.frame + "]"]) : null,
+        " ",
+        def.body,
+        mkel("br", {}, [])
+      ];
+    }) : null,
+    json.derivs ? json.derivs.map(deriv => {
+      return [
+        mkel("b", {}, [deriv.head]),
+        " ",
+        mkel("i", {}, [deriv.pos + ". "]),
+        deriv.body,
+        mkel("br", {}, [])
+      ];
+    }) : null,
+    json.used_in ? mkel("details", {}, [
+      mkel("summary", {}, ["used in (", json.used_in.length, ")"]),
+      json.used_in.map(u => {
+        return [
+          mkel("a", { "href": "?q=" + u }, [u]),
+          ", "
+        ];
+      })
+    ].flat(Infinity).slice(0, -1)) : null
+  ].flat(Infinity));
+  return entry;
 }
 function load(res, page) {
-    const start = page * 100;
-    const end = (page + 1) * 100;
-    var nodes = [];
-    for (var i = start; i < end; i++) {
-        if (res[i]) {
-            nodes.push(htmlify(res[i][0]));
-        }
+  const start = page * 100;
+  const end = (page + 1) * 100;
+  var nodes = [];
+  for (var i = start; i < end; i++) {
+    if (res[i]) {
+      nodes.push(htmlify(res[i][0]));
     }
-    $`res`.append(...nodes);
+  }
+  $`res`.append(...nodes);
 }
